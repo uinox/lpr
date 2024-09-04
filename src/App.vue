9c1e154cf96c97770f27f8a2b0436a2216f4b8a3
uinox <326138915@qq.com>
2024-09-04 21:36:29 +0800

    添加备案号

--- src/App.vue
+++ src/App.vue
@@ -3,9 +3,24 @@ import { RouterView } from 'vue-router'
 </script>
 
 <template>
-  <RouterView />
+  <div class="header">
+  </div>
+  <main style="height: 100%;">
+    <RouterView />
+  </main>
+  <div class="footer">
+    <a href="https://beian.miit.gov.cn/">京ICP备17049124</a>
+  </div>
 </template>
 
 <style scoped>
-
+.footer{
+  text-align: center;height: 50px;background-color: #fafafa;line-height: 50px;
+}
+.footer a {
+  color: #b0b0b0
+}
+.footer a:hover {
+  background-color: #fafafa;
+}
 </style>
